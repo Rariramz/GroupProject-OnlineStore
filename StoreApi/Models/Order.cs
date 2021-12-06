21a9bd609a19e512600eb683c97e760a46f8b33e
Daniel Malets <malets.d.l@gmail.com>
2021-12-06 22:14:35 +0300

    new entities

---
+++ StoreApi/Models/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StoreApi.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public decimal TotalPrice { get; set; }
+        public int UserId { get; set; }
+        public int AddressId { get; set; }
+        public DateTime InitialDate { get; set; }
+        public DateTime DeliveryDate { get; set; }
+        public bool IsDelivery { get; set; }
+}
+}
