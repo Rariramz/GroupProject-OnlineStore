8a41d346b754e742ba858f1040b1904d95742e21
vladKB1 <kazlou.uladzislau@gmail.com>
2021-12-07 23:59:12 +0300

    temp commit with header

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,7 +1,24 @@
+import { Box, Grid, Paper, Typography, Button } from "@mui/material";
+import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
+import { styled } from "@mui/styles";
 import React from "react";
 
+const BackgroundedDiv = styled(Box)(({ theme }) => ({
+  height: "10vh",
+  backgroundColor: "#f7f8fa",
+  padding: theme.spacing(10, 40),
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "space-around",
+  alignItems: "center",
+}));
+
 const Footer = () => {
-  return <></>;
+  return (
+    <BackgroundedDiv>
+      <span>text in</span>
+    </BackgroundedDiv>
+  );
 };
 
 export default Footer;
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,42 @@
+import { Box, Grid, Paper, CardMedia, Typography, Button } from "@mui/material";
+import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
+import { styled } from "@mui/styles";
 import React from "react";
 
+import logo from "../images/svg/logo.svg";
+import cart from "../images/svg/Cart.svg";
+import profile from "../images/svg/Profile.svg";
+
+const HeaderDiv = styled(Box)(({ theme }) => ({
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "space-evenly",
+  alignItems: "center",
+  padding: 20,
+}));
+
 const Header = () => {
-  return <></>;
+  return (
+    <HeaderDiv>
+      <CardMedia component="img" title="Candleaf" image={logo} />
+      <HeaderDiv width="40vw">
+        <Typography variant="body1" color="initial" textDecoration="none">
+          <a href="#">Home</a>
+        </Typography>
+        <Typography variant="body1" color="initial" textDecoration="none">
+          <a href="#">About</a>
+        </Typography>
+        <Typography variant="body1" color="initial" textDecoration="none">
+          <a href="#">Contact us</a>
+        </Typography>
+      </HeaderDiv>
+
+      <HeaderDiv>
+        <img class="cart" src={cart} alt="cart" paddingRight="20" />
+        <img class="profile" src={profile} alt="profile" />
+      </HeaderDiv>
+    </HeaderDiv>
+  );
 };
 
 export default Header;
