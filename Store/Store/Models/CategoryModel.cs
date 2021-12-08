01f82a90229636fc034697e676bb93787cfbcc64
fedjaz <fedjaz5@gmail.com>
2021-12-08 23:13:32 +0300

    fix: categories controller

---
+++ Store/Store/Models/CategoryModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Store.Models
+{
+    public class CategoryModel
+    {
+        public string Name { get; set; } = "";
+        public string Description { get; set; } = "";
+        public int ParentID { get; set; }
+        public IFormFile? Image { get; set; }
+        public IFormFile? InsideImage { get; set; }
+    }
+}
--- Store/Store/Models/CategoryResult.cs
+++ Store/Store/Models/CategoryResult.cs
@@ -8,8 +8,16 @@
 
     public static class CategoryResultConstants
     {
-        public const int ERROR_ROOT_ALREADY_EXISTS = 440;
+        public const int ERROR_PARENT_NOT_EXISTS = 440;
         public const int ERROR_PARENT_INVALID = 441;
         public const int ERROR_CHILD_CONFLICT = 442;
+        public const int ERROR_NAME_EMPTY = 443;
+        public const int ERROR_NAME_VALIDATION_FAIL = 444;
+        public const int ERROR_NAME_TOO_LONG = 445;
+        public const int ERROR_DESCRIPTION_EMPTY = 446;
+        public const int ERROR_DESCRIPTION_VALIDATION_FAIL = 447;
+        public const int ERROR_DESCRIPTION_TOO_LONG = 448;
+        public const int ERROR_IMAGE_1 = 449;
+        public const int ERROR_IMAGE_2 = 450;
     }
 }
