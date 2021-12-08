59c814be206e7d24e7434884200df9985da10c07
Skava600 <uskava7@gmail.com>
2021-12-08 18:50:53 +0300

    update: cart controller.

---
+++ Store/Store/Models/UserItemResult.cs
@@ -0,0 +1,15 @@
+﻿namespace Store.Models
+{
+    public class UserItemResult
+    {
+        public bool Success { get; set; }
+        public List<int> ErrorCodes { get; set; } = new List<int> { };
+    }
+    public static class UserItemResultConstants
+    {
+        public const int ERROR_USER_INVALID = 440;
+        public const int ERROR_COUNT_LESS_ONE = 441;
+        public const int ERROR_ACCESS_DENIED = 442;
+
+    }
+}
