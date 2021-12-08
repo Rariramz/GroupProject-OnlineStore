b4aab1d8461db97460c7e49e1aeb2740cf6579ed
vladKB1 <kazlou.uladzislau@gmail.com>
2021-12-08 13:20:14 +0300

    feat: add layout for Header & Footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,21 +3,156 @@ import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
 import { styled } from "@mui/styles";
 import React from "react";
 
-const BackgroundedDiv = styled(Box)(({ theme }) => ({
-  height: "10vh",
-  backgroundColor: "#f7f8fa",
-  padding: theme.spacing(10, 40),
+
+import logo from "../images/footer.png";
+
+const Footer1 = styled(Box)(({ theme }) => ({
   display: "flex",
   flexDirection: "row",
   justifyContent: "space-around",
   alignItems: "center",
+  padding: 10,
+  backgroundColor: "#272727",
 }));
+
+const Footer2 = styled(Box)(({ theme }) => ({
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "space-around",
+  alignItems: "center",
+  padding: 10,
+}));
+
 
 const Footer = () => {
   return (
-    <BackgroundedDiv>
-      <span>text in</span>
-    </BackgroundedDiv>
+    <>
+      <Footer1>
+        <Grid
+          item
+          container
+          spacing={3}
+          direction="column"
+          justify="center"
+          alignItems="center"
+          alignContent="center"
+          wrap="nowrap"
+        >
+          <hr width="1200" height="1.5" style={{ marginTop: 60 }}></hr>
+          <Grid
+            item
+            container
+            direction="row"
+            justify="center"
+            alignItems="center"
+            alignContent="center"
+            wrap="nowrap"
+          >
+            <Grid
+              item
+              container
+              direction="column"
+              justify="center"
+              alignItems="flex-start"
+              alignContent="center"
+              wrap="nowrap"
+            >
+              <img class="logo" src={logo} alt="Candleaf" style={{ paddingLeft: 115 }} />
+              <Typography variant="body3" color="#FFFFFF" style={{ paddingLeft: 115, width: 250 }}>
+                Your natural candle made foryour home and for your wellness.
+              </Typography>
+              <Typography variant="body4" color="#FFFFFF" style={{ paddingLeft: 115, paddingTop: 50, width: 250 }}>
+                ©Candleaf All Rights Reserved.
+              </Typography>
+            </Grid>
+
+            <Grid
+              item
+              container
+              spacing={3}
+              direction="row"
+              justify="center"
+              alignItems="flex-end"
+              alignContent="center"
+              wrap="nowrap"
+            >
+              <Grid
+                item
+                container
+                spacing={3}
+                direction="Column"
+                justify="center"
+                alignItems="flex-start"
+                alignContent="center"
+                wrap="nowrap"
+              >
+                <Typography variant="body3" color="primary">
+                  Discovery
+                </Typography>
+                <Typography variant="body3" color="#FFFFFF" style={{ paddingTop: 25 }}>
+                  New season
+                </Typography>
+                <Typography variant="body3" color="#FFFFFF" style={{ paddingTop: 20 }}>
+                  Most searched
+                </Typography>
+                <Typography variant="body3" color="#FFFFFF" style={{ paddingTop: 20 }}>
+                  Most selled
+                </Typography>
+              </Grid>
+
+              <Grid
+                item
+                container
+                spacing={3}
+                direction="Column"
+                justify="center"
+                alignItems="flex-start"
+                alignContent="center"
+                wrap="nowrap"
+              >
+                <Typography variant="body3" color="primary">
+                  About
+                </Typography>
+                <Typography variant="body3" color="#FFFFFF" style={{ paddingTop: 25 }}>
+                  Help
+                </Typography>
+                <Typography variant="body3" color="#FFFFFF" style={{ paddingTop: 20 }}>
+                  Shipping
+                </Typography>
+                <Typography variant="body3" color="#FFFFFF" style={{ paddingTop: 20 }}>
+                  Affiliate
+                </Typography>
+              </Grid>
+
+              <Grid
+                item
+                container
+                spacing={3}
+                direction="Column"
+                justify="center"
+                alignItems="flex-start"
+                alignContent="center"
+                wrap="nowrap"
+              >
+                <Typography variant="body3" color="primary">
+                  Info
+                </Typography>
+                <Typography variant="body3" color="#FFFFFF" style={{ paddingTop: 25 }}>
+                  Contact us
+                </Typography>
+                <Typography variant="body3" color="#FFFFFF" style={{ paddingTop: 20 }}>
+                  Privacy Policies
+                </Typography>
+                <Typography variant="body3" color="#FFFFFF" style={{ paddingTop: 20 }}>
+                  Terms & Conditions
+                </Typography>
+              </Grid>
+
+            </Grid>
+          </Grid>
+        </Grid>
+      </Footer1>
+    </>
   );
 };
 
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,4 +1,4 @@
-import { Box, Grid, Paper, CardMedia, Typography, Button } from "@mui/material";
+import { Box, Grid, Paper, Typography, Button } from "@mui/material";
 import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
 import { styled } from "@mui/styles";
 import React from "react";
@@ -10,30 +10,30 @@ import profile from "../images/svg/Profile.svg";
 const HeaderDiv = styled(Box)(({ theme }) => ({
   display: "flex",
   flexDirection: "row",
-  justifyContent: "space-evenly",
+  justifyContent: "space-around",
   alignItems: "center",
-  padding: 20,
+  padding: 10,
 }));
 
 const Header = () => {
   return (
     <HeaderDiv>
-      <CardMedia component="img" title="Candleaf" image={logo} />
+      <img class="logo" src={logo} alt="Candleaf" />
       <HeaderDiv width="40vw">
         <Typography variant="body1" color="initial" textDecoration="none">
-          <a href="#">Home</a>
+          Home
         </Typography>
         <Typography variant="body1" color="initial" textDecoration="none">
-          <a href="#">About</a>
+          About
         </Typography>
         <Typography variant="body1" color="initial" textDecoration="none">
-          <a href="#">Contact us</a>
+          Contact us
         </Typography>
       </HeaderDiv>
 
-      <HeaderDiv>
-        <img class="cart" src={cart} alt="cart" paddingRight="20" />
+      <HeaderDiv width="7vw">
         <img class="profile" src={profile} alt="profile" />
+        <img class="cart" src={cart} alt="cart" paddingRight="20" />
       </HeaderDiv>
     </HeaderDiv>
   );
