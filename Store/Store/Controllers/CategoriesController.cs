e152102b1a2b147e2657b9dbc3477b96a198e909
Daniel Malets <malets.d.l@gmail.com>
2021-12-08 19:44:36 +0300

    category controlller update

--- Store/Store/Controllers/CategoriesController.cs
+++ Store/Store/Controllers/CategoriesController.cs
@@ -2,17 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Store.Data;
 using Store.Entities;
+using Store.Models;
 
 namespace Store.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class CategoriesController : ControllerBase
+    public class CategoriesController : Controller
     {
         private readonly ApplicationDbContext _context;
 
@@ -23,6 +25,7 @@ namespace Store.Controllers
 
         // GET: api/Categories
         [HttpGet]
+        [Authorize("admin")]
         public async Task<ActionResult<IEnumerable<Category>>> GetCategories()
         {
             return await _context.Categories.ToListAsync();
@@ -30,6 +33,7 @@ namespace Store.Controllers
 
         // GET: api/Categories/5
         [HttpGet("{id}")]
+        [Authorize("admin")]
         public async Task<ActionResult<Category>> GetCategory(int id)
         {
             var category = await _context.Categories.FindAsync(id);
@@ -45,11 +49,37 @@ namespace Store.Controllers
         // PUT: api/Categories/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
+        [Authorize("admin")]
         public async Task<IActionResult> PutCategory(int id, Category category)
         {
+            CategoryResult categoryResult = new CategoryResult() { Success = true };
+
             if (id != category.ID)
             {
                 return BadRequest();
+            }
+
+            Category rootCategory = await _context.Categories.FirstOrDefaultAsync(c => c.ParentID == c.ID);
+
+            if (rootCategory?.ID != category.ID)
+            {
+                if (category.ID == category.ParentID)
+                {
+                    categoryResult.Success = false;
+                    categoryResult.ErrorCodes.Add(CategoryResultConstants.ERROR_ROOT_ALREADY_EXISTS);
+                }
+            }
+
+            if (!category.ParentID.HasValue || !CategoryExists(category.ParentID ?? 0))
+            {
+                categoryResult.Success = false;
+                categoryResult.ErrorCodes.Add(CategoryResultConstants.ERROR_PARENT_INVALID);
+            }
+
+            if (category.ChildItems?.Count > 0 && category.ChildCategories?.Count > 0)
+            {
+                categoryResult.Success = false;
+                categoryResult.ErrorCodes.Add(CategoryResultConstants.ERROR_CHILD_CONFLICT);
             }
 
             _context.Entry(category).State = EntityState.Modified;
@@ -70,18 +100,48 @@ namespace Store.Controllers
                 }
             }
 
-            return NoContent();
+            return Json(_context);
         }
 
         // POST: api/Categories
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
+        [Authorize("admin")]
         public async Task<ActionResult<Category>> PostCategory(Category category)
         {
-            _context.Categories.Add(category);
-            await _context.SaveChangesAsync();
+            CategoryResult categoryResult = new CategoryResult() { Success = true };
 
-            return CreatedAtAction("GetCategory", new { id = category.ID }, category);
+
+            Category rootCategory = await _context.Categories.FirstOrDefaultAsync(c => c.ParentID == c.ID);
+
+            if (rootCategory != null)
+            {
+                if (category.ID == category.ParentID)
+                {
+                    categoryResult.Success = false;
+                    categoryResult.ErrorCodes.Add(CategoryResultConstants.ERROR_ROOT_ALREADY_EXISTS);
+                }
+            }
+
+            if (!category.ParentID.HasValue || !CategoryExists(category.ParentID ?? 0))
+            {
+                categoryResult.Success = false;
+                categoryResult.ErrorCodes.Add(CategoryResultConstants.ERROR_PARENT_INVALID);
+            }
+
+            if (category.ChildItems?.Count > 0 && category.ChildCategories?.Count > 0)
+            {
+                categoryResult.Success = false;
+                categoryResult.ErrorCodes.Add(CategoryResultConstants.ERROR_CHILD_CONFLICT);
+            }
+
+            if (categoryResult.Success)
+            {
+                _context.Categories.Add(category);
+                await _context.SaveChangesAsync();
+            }
+
+            return Json(categoryResult);
         }
 
         // DELETE: api/Categories/5
